a574340da016235af99185caee72337b27cae8c6
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-01-04 21:47:09 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -9,7 +9,7 @@ function generateShortCode(): string {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let result = '';
   for (let i = 0; i < 6; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+    resul  += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return result;
 }
